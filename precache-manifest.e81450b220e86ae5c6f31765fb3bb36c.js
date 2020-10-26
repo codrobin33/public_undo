94f4e216e787d625d9b535ea06299a1862fe21ef
Cody Robinson <crobinson@locktonaffinity.com>
2020-10-26 10:43:54 -0500

    website

---
+++ precache-manifest.e81450b220e86ae5c6f31765fb3bb36c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9bf9db59cce4c1caa8675f0b3b86d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1235ea506e97a641d8f3",
+    "url": "/static/css/main.cafbd09f.chunk.css"
+  },
+  {
+    "revision": "cb10e8b57c19e41dd460",
+    "url": "/static/js/0.2eadb86e.chunk.js"
+  },
+  {
+    "revision": "c2f89df41adcac7ba418",
+    "url": "/static/js/1.5652eb02.chunk.js"
+  },
+  {
+    "revision": "1c978f5129120a1caeb2",
+    "url": "/static/js/10.a3f3d046.chunk.js"
+  },
+  {
+    "revision": "3d72107c337c672a95b9",
+    "url": "/static/js/11.bb81aea4.chunk.js"
+  },
+  {
+    "revision": "9cd6417e619077ce5e6f",
+    "url": "/static/js/12.a91c1719.chunk.js"
+  },
+  {
+    "revision": "bd7e4fbf8351a7fcb973",
+    "url": "/static/js/13.f9d2a1ca.chunk.js"
+  },
+  {
+    "revision": "632ca4805f01292ba4ec",
+    "url": "/static/js/14.afa94412.chunk.js"
+  },
+  {
+    "revision": "74104a270e45c1c834b2",
+    "url": "/static/js/15.7af9b3a6.chunk.js"
+  },
+  {
+    "revision": "b394c47219ade2cf4a68",
+    "url": "/static/js/16.8380cb0a.chunk.js"
+  },
+  {
+    "revision": "41a3c0437f451e60340f",
+    "url": "/static/js/4.0d2457eb.chunk.js"
+  },
+  {
+    "revision": "95795638ed8442da08eb",
+    "url": "/static/js/5.a9cd6e77.chunk.js"
+  },
+  {
+    "revision": "6a1f17fe00d2071b9d65",
+    "url": "/static/js/6.bb06672e.chunk.js"
+  },
+  {
+    "revision": "f3f546370a24d2301aa2",
+    "url": "/static/js/7.159f27f6.chunk.js"
+  },
+  {
+    "revision": "613a8ae8e96ceb75b9c2",
+    "url": "/static/js/8.41080a0f.chunk.js"
+  },
+  {
+    "revision": "450224f78db1dc06e3ec",
+    "url": "/static/js/9.8b068322.chunk.js"
+  },
+  {
+    "revision": "1235ea506e97a641d8f3",
+    "url": "/static/js/main.030ec12e.chunk.js"
+  },
+  {
+    "revision": "7169c0b828af93ffb9a6",
+    "url": "/static/js/runtime-main.f26a4f42.js"
+  }
+]);
---
+++ static/js/14.afa94412.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpundo_today2=this.webpackJsonpundo_today2||[]).push([[14],{39:function(e,a,t){"use strict";t.r(a);var s=t(0),n=t.n(s);a.default=function(e){return n.a.createElement("section",{className:"section has-text-centered has-background-grey-darker"},n.a.createElement("a",{className:"icon has-workflow-spacing has-text-white",href:e.link},n.a.createElement("i",{className:"fas fa-angle-double-up"})),n.a.createElement("h5",{className:"title is-6 has-text-white"},"COPYRIGHT \xa9 2019 UNDO SKINCARE CENTER."))}}}]);
+//# sourceMappingURL=14.afa94412.chunk.js.map
